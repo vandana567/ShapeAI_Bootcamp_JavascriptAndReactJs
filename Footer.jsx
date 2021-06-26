9f5620da7dcd9159df8423253153ed8b6bfaf6be
vandana567 <45798170+vandana567@users.noreply.github.com>
2021-06-26 22:03:14 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <p>Copyright by Shape AI @ {new Date().getFullYear()}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
